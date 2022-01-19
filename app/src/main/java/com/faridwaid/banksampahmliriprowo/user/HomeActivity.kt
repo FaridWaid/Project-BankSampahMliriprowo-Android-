db5561c2affea3edd0e6365b63a54d3fde95687a
Achmad Farid Alfa Waid <apakjr11@gmail.com>
2022-01-19 21:55:22 +0700

    setup bottom navigation

--- app/src/main/java/com/faridwaid/banksampahmliriprowo/user/HomeActivity.kt
+++ app/src/main/java/com/faridwaid/banksampahmliriprowo/user/HomeActivity.kt
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.faridwaid.banksampahmliriprowo.LoginActivity
 import com.faridwaid.banksampahmliriprowo.R
 import com.google.firebase.auth.FirebaseAuth
+import me.ibrahimsn.lib.SmoothBottomBar
 
 class HomeActivity : AppCompatActivity() {
 
@@ -25,14 +31,36 @@ class HomeActivity : AppCompatActivity() {
         // Mengisi variabel auth dengan fungsi yang ada pada FirebaseAuth
         auth = FirebaseAuth.getInstance()
 
+        // Mendefinisikan NavController yang nantinya akan digunakan untuk control fragment
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+
+        // appBarConfiguration digunakan untuk item menu pada toolbar akan berubah jika salah satu menu diklik
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_home, R.id.nav_profile
+        ).build()
+
+        // setup nav controller dan app bar
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // setup smooth bar menu
+        val popUpMenu =  PopupMenu(this, null)
+        popUpMenu.inflate(R.menu.bottom_nav_menu)
+        val menu = popUpMenu.menu
+        val navBottom: SmoothBottomBar = findViewById(R.id.nav_bottom)
+        navBottom.setupWithNavController(menu, navController)
+
+
     }
 
+    // fungsi untuk menampilkan menu option
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.option_menu, menu)
         return true
     }
 
+    // fungsi "onOptionsItemSelected" adalah fungsi ketika salah satu option ditekan maka akan do something
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // ketika option dengan id "logout" ditekan maka akan pindah intent/activity
         when(item.itemId){
             R.id.logout ->{
                 auth.signOut()
